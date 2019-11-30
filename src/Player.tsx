8a9d68c5a52d3aed0a8d6009f8a9a7744bb1f5c1
asuha isik <aisik@smartover.net>
2019-11-30 12:33:42 +0300

    end of day

--- src/Player.tsx
+++ src/Player.tsx
@@ -4,13 +4,15 @@ import ReactPlayer from 'react-player'
 export const Player: React.FC = () => {
   return (
     <div className="player">
-      <ReactPlayer url='https://www.yotube.com/watch?v=4V7oq8KtDsA' playing controls={true} className="video" />
-      <div className="video-overlay"
+      <ReactPlayer url='https://www.youtube.com/watch?v=4V7oq8KtDsA' playing controls={true} className="video" />
+      <div className="player-overlay"
         style={{
           position: 'relative',
           width: '100%',
           height: '100%'
-        }}></div>
+        }}>
+          abc
+        </div>
     </div>
   )
 
--- src/components/OverlayPortal.tsx
+++ src/components/OverlayPortal.tsx
@@ -1,26 +1,37 @@
-import React, { useRef, useEffect, useCallback } from 'react';
+import React, { useRef, useEffect, useCallback, useState } from 'react';
 import ReactDOM from 'react-dom';
 import ResizeSensor from 'css-element-queries/src/ResizeSensor';
 import { throttle } from 'lodash';
 
 const aspectRatio = 1.777;
-const modalRoot = document.getElementsByClassName('video-overlay')[0] as HTMLDivElement;
+const modalRoot = document.getElementsByClassName('player-overlay')[0] as HTMLDivElement;
+console.log(modalRoot);
 
 export const OverlayPortal: React.FC<{ children: any }> = ({ children }) => {
-  
+  const [root, setRoot] = useState();
+
+  useEffect(() => {
+    setRoot(document && document.getElementsByClassName('player-overlay')[0] as HTMLDivElement)
+
+  }, [])
+
   const containerRef = useRef<HTMLDivElement>(null);
   // Set The Context Api
-  return ReactDOM.createPortal(
-    <div
-      ref={containerRef}
-      className="vb--overlay-container"
-      style={{
-        position: 'relative',
-        width: '100%',
-        height: '100%',
-        pointerEvents: 'none'
-      }}>
-      {children}
-    </div>
-    , modalRoot)
+  if (root) {
+    console.log(root);
+    return ReactDOM.createPortal(
+      <div
+        ref={containerRef}
+        className="overlay-container"
+        style={{
+          position: 'relative',
+          width: '100%',
+          height: '100%',
+          pointerEvents: 'none'
+        }}>
+        {children}
+      </div>
+      , root)
+  }
+  else return null
 }
